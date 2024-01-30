0a15385a2dafc0515c38142e01fa9480d929058e
vamshi470pagidi <157589288+vamshi470pagidi@users.noreply.github.com>
2024-01-30 22:30:39 +0530

    Update application_start.sh

--- Flask_app_CICD-0.1/scripts/application_start.sh
+++ Flask_app_CICD-0.1/scripts/application_start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 echo "Running container..."
-docker run --name flask_app -d -p 5000:5000 123456789.dkr.ecr.us-east-1.amazonaws.com/flask_image:latest
+docker run --name flask_app -d -p 5000:5000 043858253761.dkr.ecr.us-east-1.amazonaws.com/flask_image:latest
