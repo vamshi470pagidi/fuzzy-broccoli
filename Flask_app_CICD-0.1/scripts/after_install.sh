7df282762e585d608d52bbeab6eca571ecacab20
vamshi470pagidi <157589288+vamshi470pagidi@users.noreply.github.com>
2024-01-30 22:24:51 +0530

    Update after_install.sh

--- Flask_app_CICD-0.1/scripts/after_install.sh
+++ Flask_app_CICD-0.1/scripts/after_install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 echo "Logging in to Amazon ECR..."
-docker login --username AWS --password $(aws ecr get-login-password --region us-east-2) 123456789.dkr.ecr.us-east-1.amazonaws.com
+aws ecr get-login-password --region us-east-1 | docker login --username AWS --password-stdin 043858253761.dkr.ecr.us-east-1.amazonaws.com
 echo "Logged in to Amazon ECR successfully."
 
 echo "Pulling image from Amazon ECR"
-docker pull 123456789.dkr.ecr.us-east-1.amazonaws.com/flask_image:latest
+docker pull 043858253761.dkr.ecr.us-east-1.amazonaws.com/flask_image:latest
 echo "Pulled image from Amazon ECR successfully."
